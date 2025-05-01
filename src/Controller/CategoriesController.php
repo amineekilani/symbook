847a9edfbae16a04aa045cfcc8bc9145ade40c31
Amine Kilani <aminekilani901@gmail.com>
2025-05-01 21:44:09 +0100

    01/05 v1

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -6,37 +6,46 @@ use App\Entity\Categories;
 use App\Form\CategorieType;
 use App\Repository\CategoriesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-final class CategoriesController extends AbstractController{
-    #[Route('admin/categories', name: 'app_categories')]
+final class CategoriesController extends AbstractController
+{
+    #[Route('/admin/categories', name: 'admin_categories')]
     public function index(CategoriesRepository $rep): Response
     {
         $categories=$rep->findAll();
-        return $this->render('categories/index.html.twig', [
-            'categories' => $categories
-        ]);
+        return $this->render('categories/index.html.twig', ['categories' => $categories]);
     }
-    //create
     #[Route('/admin/categories/create', name: 'app_categories_create')]
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request,EntityManagerInterface $em): Response
     {
         $categorie=new Categories();
-        $form=$this->createForm(CategorieType::class, $categorie);
+        $form=$this->createForm(CategorieType::class,$categorie);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
+        if($form->isSubmitted() && $form->isValid()){
             $em->persist($categorie);
             $em->flush();
-            $this->addFlash('success', 'Category created successfully!');
-            
-            return $this->redirectToRoute("app_categories");
+            $this->addFlash('success','la categorie a été bien ajouter');
+            return $this->redirectToRoute('admin_categories');
         }
         return $this->render('categories/create.html.twig', [
-            'form' => $form
+            'form' => $form,
         ]);
+    }
+    #[Route('/admin/categories/update/{id}', name: 'app_categories_update')]
+    public function update(Request $request,EntityManagerInterface $em,Categories $categorie): Response
+    {
+        $form=$this->createForm(CategorieType::class,$categorie);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+            $this->addFlash('success','La categorie a été bien modifier');
+            return $this->redirectToRoute('admin_categories');
+        }
+        return $this->render('categories/update.html.twig', ['form' => $form]);
     }
 }
--- src/Controller/LivresController.php
+++ src/Controller/LivresController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
 use App\Entity\Livres;
+use App\Form\CategorieType;
+use App\Form\LivresType;
 use App\Repository\LivresRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -13,85 +16,82 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class LivresController extends AbstractController
 {
-    #[Route('/admin/livres', name: 'app_livres')]
-    public function all(LivresRepository $livresRepository, PaginatorInterface $paginator, Request $request): Response
-    {
-        $livres=$livresRepository->findAll();
-        $livres=$paginator->paginate($livresRepository->findAll(), $request->query->getInt('page', 1), 10);
-        return $this->render('livres/index.html.twig', [
-            'livres' => $livres
-        ]);
-    }
-    #[Route('/admin/livres/create', name: 'app_livres_create')]
-    public function create(EntityManagerInterface $em): Response
-    {
-        $livre=new Livres();
-        $livre->setTitre('Le Seigneur des Anneaux');
-        $livre->setIsbn('978-2266144342');
-        $livre->setSlug('le-seigneur-des-anneaux');
-        $livre->setImage('images/seigneur-anneaux.jpg');
-        $livre->setResume('Une épopée fantastique dans un monde remplié de magie, de héros et de créatures mythiques.');
-        $livre->setEditeur('Christian Bourgois');
-        $livre->setDateedition(new \DateTime('1954-07-29'));
-        $livre->setPrix(19.99);
-        $em->persist($livre);
-        $em->flush();
-        return new Response("Livre ".$livre->getId()." créé avec succès");
-    }
-    #[Route('/admin/livres/show', name: 'app_livres_showAll')]
-    public function showAll(LivresRepository $livresRepository): Response
-    {
-        $livres=$livresRepository->findAll();
-        if (!$livres)
-        {
-            throw $this->createNotFoundException("Aucun livre trouvé");
-        }
-        dd($livres);
-    }
-    #[Route('/admin/livres/show/{id}', name: 'app_livres_show')]
+
+    #[Route('admin/livres/show/{id}', name: 'app_livres_show')]
     public function show(Livres $livre): Response
-    // Param convertor
     {
-        return $this->render('livres/detail.html.twig', [
-            'livre' => $livre
-        ]);
+       if(!$livre){
+           throw $this->createNotFoundException("livre not found");
+       }
+        return $this->render('livres/detail.html.twig', ['livre'=>$livre]);
+
     }
-    #[Route('/admin/livres/show2', name: 'app_livres_show2')]
-    public function show2(LivresRepository $livresRepository): Response
+    #[Route('admin/livres/show2', name: 'app_livres_show2')]
+    public function show2(LivresRepository $rep): Response
     {
-        $livre=$livresRepository->findOneBy(['titre'=>'Le Seigneur des Anneaux']);
-        if (!$livre)
-        {
-            throw $this->createNotFoundException("Livre non trouvé");
+        $livre=$rep->findOneBy(['titre'=>'Titre 1','editeur'=>'Cérès Éditions']);
+        if(!$livre){
+            throw $this->createNotFoundException("livre not found");
         }
         dd($livre);
     }
-    #[Route('/admin/livres/show3', name: 'app_livres_show3')]
-    public function show3(LivresRepository $livresRepository): Response
+    #[Route('admin/livres/show3', name: 'app_livres_show3')]
+    public function show3(LivresRepository $rep): Response
     {
-        $livres=$livresRepository->findBy(['titre' => 'Le Seigneur des Anneaux'],
-        ['prix' => 'ASC']);
-        if (!$livres)
-        {
-            throw $this->createNotFoundException("Livre non trouvé");
+        $livres=$rep->findBy(['titre'=>'Titre 1'],['prix'=>'DESC']);
+        if(!$livres){
+            throw $this->createNotFoundException("livre not found ");
         }
         dd($livres);
     }
-    #[Route('/admin/livres/delete/{id}', name: 'app_livres_delete')]
-    public function delete(EntityManagerInterface $em, Livres $livre): Response
+    #[Route('admin/livres', name: 'app_livres_all')]
+    public function all(LivresRepository $rep,PaginatorInterface $paginator, Request $request): Response
     {
+        $livres= $paginator->paginate(
+            $rep->findAll(),
+            $request->query->getInt('page', 1), 10
+        );
+        return $this->render('livres/index.html.twig', ['livres'=>$livres]);
+    }
+    #[Route('admin/livres/delete/{id}', name: 'app_livres_delete')]
+    public function delete(LivresRepository $rep,EntityManagerInterface $em,Livres $livre): Response
+    {
+        if(!$livre){
+            throw $this->createNotFoundException("livre not found");
+        }
         $em->remove($livre);
         $em->flush();
-        dd($livre);
-        return new Response("Livre $id supprimé avec succès");
+        return $this->redirectToRoute('app_livres_all');
     }
-    #[Route('/admin/livres/update/{id}', name: 'app_livres_update')]
-    public function update(Livres $livre, EntityManagerInterface $em): Response
+    #[Route('admin/livres/update/{id}', name: 'app_livres_update')]
+    public function update(LivresRepository $rep,EntityManagerInterface $em,Livres $livre): Response
     {
-        $nvPrix=$livre->getPrix()*1.1;
-        $livre->setPrix($nvPrix);
+        if(!$livre){
+            throw $this->createNotFoundException("livre not found");
+        }
+        $nouveauPrix=$livre->getPrix()*1.1;
+        $livre->setPrix($nouveauPrix);
+        $em->persist($livre);
         $em->flush();
-        dd($livre);
-        return new Response("Livre $id mis à jour avec succès");
+        return $this->redirectToRoute('app_livres_all');
+    }
+    #[Route('/admin/livres/create', name: 'app_livres_create')]
+    public function create(Request $request,EntityManagerInterface $em): Response
+    {
+        $livre=new Livres();
+        //affichage du formulaire
+        $form=$this->createForm(LivresType::class, $livre);
+        //traitement des données issues
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //dd($categorie);
+            $em->persist($livre);
+            $em->flush();
+            $this->addFlash('success','Le livre a été bien ajouté');
+            return $this->redirectToRoute('app_livres_all');
+        }
+        return $this->render('livres/create.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
