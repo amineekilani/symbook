54ac38665cd9ec9019575d5efb34e1c93109b5e3
Amine Kilani <aminekilani901@gmail.com>
2025-05-08 15:45:54 +0100

    08/05 v1

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
-use App\Form\Categorie1Type;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/categorie')]
+#[Route('/admin/categorie')]
 final class CategorieController extends AbstractController{
     #[Route(name: 'app_categorie_index', methods: ['GET'])]
     public function index(CategorieRepository $categorieRepository): Response
@@ -25,7 +25,7 @@ final class CategorieController extends AbstractController{
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $categorie = new Categorie();
-        $form = $this->createForm(Categorie1Type::class, $categorie);
+        $form = $this->createForm(CategorieType::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -52,7 +52,7 @@ final class CategorieController extends AbstractController{
     #[Route('/{id}/edit', name: 'app_categorie_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Categorie $categorie, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(Categorie1Type::class, $categorie);
+        $form = $this->createForm(CategorieType::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
